57ed6455d1068a21c17a130187827d6adc456616
Douriann <scoclox@gmail.com>
2026-04-26 20:53:39 -0400

    fead(user): create mock data for user screens & styles

---
+++ src/hooks/mock-data/mock-user.tsx
@@ -0,0 +1,13 @@
+import { User, UserRole, UserState } from '../../types/user.types';
+
+export const mockUser: User = {
+  id: '550e8400-e29b-41d4-a716-446655440000',
+  username: 'Douriann',
+  role: UserRole.USER,
+  email: 'adrian@example.com',
+  bio: 'Hola tengo hambre.',
+  account_settings: { theme: 'dark', notifications: true },
+  profile_pic: 'https://i.pravatar.cc/150?u=adrian',
+  state: UserState.ACTIVE,
+  created_at: new Date().toISOString(),
+};
