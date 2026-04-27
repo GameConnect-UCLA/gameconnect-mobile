aa41bf0449efae5a0f0155db8b1f29842b3a5eb8
hanumonke <hanumansanchez@gmail.com>
2026-04-27 19:02:58 -0400

    feat(splash-screen): add plain text splash screen with text 'Splash Screen'

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,13 @@
-import { Redirect } from "expo-router";
+import { Text,View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-  // In a real app, logic here would check if user is authenticated
-  // and redirect to (tabs) or (auth) accordingly.
-  return <Redirect href="/login" />;
+  // // In a real app, logic here would check if user is authenticated
+  // // and redirect to (tabs) or (auth) accordingly.
+  // return <Redirect href="/login" />;
+  return (<>
+  <SafeAreaView>
+    <Text>Splash Screen</Text>
+  </SafeAreaView>
+  </>)
 }
